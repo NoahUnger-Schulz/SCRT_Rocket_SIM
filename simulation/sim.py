b854f1a7f07cefe29cc5de34902d6feed9900a2b
Llarence <1010kai.t1010@gmail.com>
2025-10-10 10:11:10 -0700

    organize

--- simulation/sim.py
+++ simulation/sim.py
@@ -1,3 +1,5 @@
+# pylint: skip-file
+
 import matplotlib.pyplot as plt
 import numpy as np
 from math import *
---
+++ tests/sanity.py
@@ -0,0 +1,9 @@
+"""
+A test file for testing that github actions work
+"""
+
+
+def test_sanity():
+    """
+    A test function for testing that github actions work
+    """
